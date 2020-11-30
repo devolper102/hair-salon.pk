df6d05fbf53e54a6bd046de87399efda7a449acf
monis-irfan69 <monisirfan6677#gmail.com>
2020-11-30 17:42:16 +0500

    pull the new code

---
+++ resources/views/hair-saloon.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Hairsaloon</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
+
+	<link rel="stylesheet" type="text/css" href="css/utility.css">
+	<link rel="stylesheet" type="text/css" href="css/test.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/hair_saloon.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+	<div id="app">
+		<sliderrecommended-section></sliderrecommended-section>
+		<other-hairsaloon-list></other-hairsaloon-list>
+        <popularcategories-section></popularcategories-section> 
+		<footer-section></footer-section>
+	</div>	
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/hair-saloon.js"></script>
+</body>
+</html>
